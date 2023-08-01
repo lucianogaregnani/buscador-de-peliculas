4587b99283b1c5b98aa4d4f53faa6f4b31e08e6c
lucianogaregnani <luciano.garegnani@estudiantes.unahur.edu.ar>
2023-08-01 03:23:33 -0300

    Estructura de la página con Vite y estilo predefinidos con Water.css, ademas deun buscador agregado y peliculas mostradas

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import './App.css'
+import moviesResponse from './mocks/moviesResponse.json'
+import noResponseMovies from './mocks/noResponseMovies.json'
+
+function App() {
+  const movies = moviesResponse.Search
+  const hasMovies = movies.length > 0
+
+  return (
+    <div>
+      <header>
+        <h1>Buscador de peliculas</h1>
+        <form>
+          <input type="text" placeholder='Buscar pelicula' />
+          <button>Buscar</button>
+        </form>
+      </header>
+      <main>
+        {
+          hasMovies ? (
+            <ul>
+              {
+                movies.map(movie =>
+                  <li key={movie.imdbID}>
+                    <h3>{movie.Title}</h3>
+                    <p>{movie.Year}</p>
+                    <img src={movie.Poster} />
+                  </li>
+                )
+              }
+            </ul>
+          ) :
+          <p>{noResponseMovies.Error}</p>
+        }
+      </main>
+    </div>
+  )
+}
+
+export default App
